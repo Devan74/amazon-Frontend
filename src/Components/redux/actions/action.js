968dff99ac78ca992525a00effe67506a0838fc5
Devan74 <raghavadeva1903@gmail.com>
2023-04-12 20:25:48 +0530

    first commit

---
+++ src/Components/redux/actions/action.js
@@ -0,0 +1,18 @@
+import {URL} from '../../../App';
+
+export const getProducts = ()=> async(dispatch)=>{
+    try {
+        const data = await fetch(`${URL}/getproducts`,{
+                method:"GET",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+        });
+
+        const res = await data.json();
+        // console.log(res);
+        dispatch({type:"SUCCESS_GET_PRODUCTS",payload:res});
+    } catch (error) {
+        dispatch({type:"FAIL_GET_PRODUCTS",payload:error.reseponse});
+    }
+}
